787fd6c48685c990579e15aefb831a47a4b23322
Anton1636 <you@example.comhobermac228@gmail.com>
2023-12-12 23:04:36 +0200

    updated

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+const hre = require('hardhat')
+
+async function main() {
+	const Upload = await hre.ethers.getContractFactory('Upload')
+	const upload = await Upload.deploy()
+
+	console.log('Library deployed to:', upload.target)
+}
+
+main().catch(error => {
+	console.error(error)
+	process.exitCode = 1
+})
